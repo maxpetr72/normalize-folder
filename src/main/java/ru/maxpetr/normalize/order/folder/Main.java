a06c80bae6136ddb43c6c7c31e6a02211cb4a7e0
Maxim Petruchenko <maxpetr@list.ru>
2020-07-23 16:31:59 +0300

    Begin of moven project with 1 main class and print to comand line <Hello World>

---
+++ src/main/java/ru/maxpetr/normalize/order/folder/Main.java
@@ -0,0 +1,11 @@
+package ru.maxpetr.normalize.order.folder;
+
+/**
+ *
+ * @author Maxim Petruchenko <maxpetr@list.ru>
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
